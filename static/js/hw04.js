c99373d36753f204d4f0f7eb8bb49c79ffdcc39d
Nikhil Khandelwal <nikhilkhandelwal2021@u.northwestern.edu>
2022-05-25 14:16:28 -0500

    CSS Update to navbar + Helper function added.

--- static/js/hw04.js
+++ static/js/hw04.js
@@ -3,3 +3,19 @@ function createHTMLElement(withHtml) {
     tempElement.innerHTML = withHtml;
     return tempElement.firstElementChild
 }
+
+function getCookie(key) {
+    let name = key + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
