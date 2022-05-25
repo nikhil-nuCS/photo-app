6704d24eb957c8aa9402b9d6b2a9c065b85e31ff
Nikhil Khandelwal <nikhilkhandelwal2021@u.northwestern.edu>
2022-05-25 13:45:20 -0500

    Updating app.py

--- app.py
+++ app.py
@@ -71,6 +71,16 @@ def api_docs():
         url_root=request.url_root[0:-1] # trim trailing slash
     )
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    return r
 
 # enables flask app to run using "python3 app.py"
 if __name__ == '__main__':
